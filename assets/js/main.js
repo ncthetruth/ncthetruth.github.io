62f68fda651c5e113e96671db0fc70d77ec13db4
Nicholas Christopher <95544423+ncthetruth@users.noreply.github.com>
2023-11-05 13:14:23 +0700

    Update main.js
    adding title Icon

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,11 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+
+var linkElement = document.createElement('link');
+linkElement.rel = 'shortcut icon';
+linkElement.href = 'images/miko_0.25x.png';
+document.head.appendChild(linkElement);
   
 document.addEventListener("contextmenu", function(e) {
 	e.preventDefault();
