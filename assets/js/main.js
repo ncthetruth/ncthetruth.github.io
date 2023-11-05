a675145533725cb75bd03c0f7d19453473aec844
Nicholas Christopher <95544423+ncthetruth@users.noreply.github.com>
2023-11-05 13:14:39 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Landed by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 var linkElement = document.createElement('link');
 linkElement.rel = 'shortcut icon';
 linkElement.href = 'images/miko_0.25x.png';
